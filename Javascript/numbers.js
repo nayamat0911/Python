7721990f036347dd594514646822134870be0f3d
Nayamat Ullah <nayamat0911@gmail.com>
2022-07-14 23:04:58 +0600

    add new number

--- Javascript/numbers.js
+++ Javascript/numbers.js
@@ -4,3 +4,18 @@ let y =3;
 // not a number is a number 
 // if type e number is a number 
 // lergest number is a limited thats disite  
+
+//numbers method
+// new Set
+// add()
+// delete()
+// has()
+// forEach()
+// size()
+const mySet  = new Set()
+mySet.add("a");
+mySet.add("b");
+mySet.add("c");
+console.log(mySet)
+mySet.add('d')
+console.log(mySet)
